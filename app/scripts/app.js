f16456563a725802675536cabff35f38eae90d3d
Fabricio Flores <fabrifloresg@gmail.com>
2017-04-24 00:40:05 +0200

    remove mocked data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -51,106 +51,13 @@ angular
         controllerAs: 'vmSearch',
         resolve: {
           rooms: function($stateParams, SearchService, $state){
-          //   return SearchService.searchRooms($stateParams.city,
-          //                                    $stateParams.zipcode)
-          //  .then(function success(response){
-          //    return response;
-          //  }, function error(){
-          //    $state.go('main');
-          // });
-          var x = [
-            {
-                hotelId: 1,
-                name: 'hotel1',
-                addressLine: 'address line 1',
-                city: 'city 1',
-                province: 'province 1',
-                zipCode: '111111',
-                rooms: [
-                    {
-                        id: 1,
-                        hotelId: 1,
-                        roomType: 'Individual',
-                        price: 123123,
-                        singleBedCount: 1,
-                        doubleBedCount: 0,
-                        queenBedCount: 0,
-                        kingBedCount: 0
-                    },
-                    {
-                        id: 2,
-                        hotelId: 1,
-                        roomType: 'Double',
-                        price: 234234,
-                        singleBedCount: 0,
-                        doubleBedCount: 1,
-                        queenBedCount: 0,
-                        kingBedCount: 0
-                    },
-                    {
-                        id: 2,
-                        hotelId: 1,
-                        roomType: 'Double',
-                        price: 234234,
-                        singleBedCount: 0,
-                        doubleBedCount: 1,
-                        queenBedCount: 0,
-                        kingBedCount: 0
-                    },
-                    {
-                        id: 2,
-                        hotelId: 1,
-                        roomType: 'Double',
-                        price: 234234,
-                        singleBedCount: 0,
-                        doubleBedCount: 1,
-                        queenBedCount: 0,
-                        kingBedCount: 0
-                    },
-                    {
-                        id: 2,
-                        hotelId: 1,
-                        roomType: 'Double',
-                        price: 234234,
-                        singleBedCount: 0,
-                        doubleBedCount: 1,
-                        queenBedCount: 0,
-                        kingBedCount: 0
-                    }
-                ]
-            },
-            {
-                hotelId: 2,
-                name: 'hotel2',
-                addressLine: 'address line 2',
-                city: 'city 2',
-                province: 'province 2',
-                zipCode: '222222',
-                rooms: [
-                    {
-                        id: 3,
-                        hotelId: 2,
-                        roomType: 'Individual',
-                        price: 123123,
-                        singleBedCount: 1,
-                        doubleBedCount: 0,
-                        queenBedCount: 0,
-                        kingBedCount: 0
-                    },
-                    {
-                        id: 4,
-                        hotelId: 2,
-                        roomType: 'Double',
-                        price: 234234,
-                        singleBedCount: 0,
-                        doubleBedCount: 1,
-                        queenBedCount: 0,
-                        kingBedCount: 0
-                    }
-                ]
-            }
-        ];
-        return x;
+            return SearchService.searchRooms($stateParams.city,
+                                             $stateParams.zipcode)
+           .then(function success(response){
+             return response;
+           }, function error(){
+             $state.go('main');
+          });
           }
         }
       })
